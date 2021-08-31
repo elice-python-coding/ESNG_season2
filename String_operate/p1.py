b7cc43b86cf78a24fccc8ae2d3b6c6ac2f33fbfb
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-09-01 03:31:19 +0900

    1

---
+++ String_operate/p1.py
@@ -0,0 +1,11 @@
+def isPalindrome(s):
+    answer = "true"
+    sl = s.lower().strip()
+    s_arr = list(filter(lambda i:i.isalnum(), sl))
+    leng = len(s_arr)
+    for i in range(leng):
+        if s_arr[i] != s_arr[leng - 1 - i]:
+            return "false"
+    return answer
+
+print(isPalindrome("race a car"))
