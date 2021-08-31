1f621754f2069b3af98a58e67d9032b9031a7cea
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-09-01 03:34:17 +0900

    1

--- String_operate/p1.py
+++ String_operate/p1.py
@@ -3,7 +3,7 @@ def isPalindrome(s):
     sl = s.lower().strip()
     s_arr = list(filter(lambda i:i.isalnum(), sl))
     leng = len(s_arr)
-    for i in range(leng):
+    for i in range(leng//2):
         if s_arr[i] != s_arr[leng - 1 - i]:
             return "false"
     return answer
