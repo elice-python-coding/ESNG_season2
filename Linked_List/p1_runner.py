771627a42e4e806bef218ac1fb704140c9e66ec8
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-09-13 22:21:32 +0900

    mergeTwoLists

--- Linked_List/p1_runner.py
+++ Linked_List/p1_runner.py
@@ -1,9 +1,9 @@
 def isPalinedrome(self, head:ListNode)->bool:
     rev = None
-    slow = fast = head
+    slow = fast = head #slow와 fast모두 head부터 시작
     while fast and fast.next:
-        fast = fast.next.next
-        rev, rev.next, slow = slow, rev, slow.next
+        fast = fast.next.next #fast는 next의next
+        rev, rev.next, slow = slow, rev, slow.next ##rev는 slow, rev.next는 rev(slow.next를 rev), slow는 slow.next
     
     if fast:
         slow = slow.next
