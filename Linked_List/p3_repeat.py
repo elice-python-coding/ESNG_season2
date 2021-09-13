766373ba35fe10c2b6c112adbceb3183c6bdb529
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-09-13 22:30:15 +0900

    reverseList

---
+++ Linked_List/p3_repeat.py
@@ -0,0 +1,7 @@
+def reverseList(self, head:ListNode) -> ListNode:
+    node, prev = head, None
+
+    while node:
+        next, node.next = node.next,prev
+        prev, node = node, next
+    return prev
